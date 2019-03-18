4c4a37e167edc806ea083718755a1c74e231b556
wangyi <wangyi@zhoupu123.com>
2019-03-18 16:53:37 +0800

    1. add: 添加写入csv文件示例

--- basic/csv.py
+++ basic/csv.py
@@ -13,3 +13,11 @@ def load_csv():
         next(csv_reader)
         for row in csv_reader:
             print(row)
+
+
+def write_csv():
+    rows = [('name1', 'karl'), ('name2', 'wang')]
+    # newline解决写入文件有空行的问题，encoding解决csv打开文件乱码问题
+    with open('预测.csv', 'w', encoding='utf_8_sig', newline='') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerows(rows)
